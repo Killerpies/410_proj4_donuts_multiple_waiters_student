5307eb9540d789e115f6f80d05c27ec04efea599
justin.sanders.20 <justin.sanders.20@cnu.edu>
2022-11-24 12:24:03 -0500

    started initial setup

--- baker/Baker.cpp
+++ baker/Baker.cpp
@@ -6,12 +6,38 @@
 
 using namespace std;
 
+
 Baker::Baker(int id):id(id)
 {
 }
 
+/**
+	 * gets the number of donuts to make from anOrder, and packages that
+	 * number in boxes, 12 to a box, with a possibly partially filled box
+	 * ex. if anOrder.number_donuts =13, this function will create 2 boxes;
+	 *     1 with 12 donuts, 1 with 1
+	 * boxes then appended to anOrder.boxes
+	 * anOrder is then complete and should be appended to order_out_Vector
+	 * @param anOrder  the order to bake box and append to order_out_Vector
+	 */
 void Baker::bake_and_box(ORDER &anOrder) {
 }
 
+/**
+	 * runs a loop
+	 * efficiently sleeps as long as their are no orders to process and all_work_done is false
+	 * will be awakened whenever a waiter places an order or when a waiter finishes and then;
+	 *   if orders in order_in_Q, gets an order, and passes it to bake_and_box above
+	 *   if all waiters are finished (all_work_done==true) and there are no more orders then baker exits
+	 *
+	 * You will use cv_order_inQ to be notified by waiters
+	 * all_work_done handles the case where a baker starts before any waiters
+	 * (in which case there are no orders in the queue but all_work_done indicates
+	 * some are coming when the waiters start) as well as when all the waiters have finished
+	 */
 void Baker::beBaker() {
+	unique_lock<mutex> lck(mutex_order_inQ);
+	while (order_in_Q.empty()) {
+		cv_order_inQ.wait(lck);
+	}
 }
--- waiter/waiter.cpp
+++ waiter/waiter.cpp
@@ -1,21 +1,62 @@
 #include <string>
 #include "stdlib.h"
-
 #include "../includes/waiter.h"
 #include "../includes/externs.h"
 #include "../includes/PRINT.h"
+#include "../includes/box.h"
 
 using namespace std;
 
+/**
+	 * Make sure you increment num_working_waiters to indicate that a waiter has started
+	 * num_working_waiters is a global but it is atomic so you do not need to protect it
+	 * Also set all_work_done = false to indicate that there is work to do
+	 * all_work_done is a global but it is atomic so you do not need to protect it
+	 * @param id a number used to identify this particular waiter
+	 */
 Waiter::Waiter(int id):id(id){
-
+	num_working_waiters +=1;
+	all_work_done = false;
 }
 
+/**
+	 * gets next Order from global file_IO object
+	 * file_IO has its own internal locking so you
+	 * do not need to synchronize access to it in this function
+	 * @param anOrder
+	 * @return if SUCCESS then anOrder contains new order
+	 *         otherwise return contains fileIO error
+	 */
 int Waiter::getNext(ORDER &anOrder){
+	return m_io.getNext(anOrder);
 
 }
 
+/**
+	 * Runs a loop that calls getNext(ORDER &anOrder) which
+	 * gets orders from global file_IO one at a time
+	 * puts them in order_in_Q then signals baker(s) using cv_order_inQ
+	 * so they can be consumed by baker(s)
+	 * when finished exits loop, decrements num_working_waiters checks to see if
+	 * num_working_waiters==0 and if so sets all_work_done=true
+	 * then signals baker(s) using cv_order_inQ that it is done
+	 */
 void Waiter::beWaiter() {
+	while (!all_work_done){
+		unique_lock<mutex> lck(mutex_order_inQ);
+		ORDER temp;
+		int hasLeft = this->getNext(temp);
+		if (hasLeft == NO_ORDERS) {
+			all_work_done=true;
+			num_working_waiters -=1;
+		}
+		else {
+			order_in_Q.push(temp);
+			cv_order_inQ.notify_all();
+		}
+	}
+
+
 }
 
 
